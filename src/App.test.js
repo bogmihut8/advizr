b0b1b861ba2edc956a9568827f4ebff565a1cc5a
bogdan.mihut8 <bogdan.mihut8@gmail.com>
2019-03-25 19:39:17 -0400

    tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,58 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import { expect, assert } from 'chai';
 import App from './App';
+import Prompt from './components/Prompt';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './store/reducer'
+
+const store = createStore(rootReducer)
 
 it('renders without crashing', () => {
   shallow(<App />);
 });
+
+const wrapper = mount(<Provider store={store}><App /></Provider>);
+
+it('renders clients container', () => {
+    const clients = wrapper.find('.clients');
+    assert.equal(clients.length, 1);
+  });
+
+it('renders add-clients button', () => {
+    const button = wrapper.find('.add-client');
+    assert.equal(button.length, 1);
+  });
+
+it('renders at least a client', () => {
+    const clients = wrapper.find('.clients .client');
+    assert.notEqual(clients.length, 0);
+});
+
+
+
+
+it('renders Prompt', () => {
+    const prompt = shallow(<Prompt />);
+    assert.equal(prompt.length, 1);
+  });
+
+it('renders prompt previous button', () => {
+    const prompt = shallow(<Prompt />);
+    const previous = wrapper.find('.control-buttons .previous');
+    assert.equal(previous.length, 1);
+});
+
+it('renders prompt next button', () => {
+    const prompt = shallow(<Prompt />);
+    const next = wrapper.find('.control-buttons .next');
+    assert.equal(next.length, 1);
+});
+
+it('should open prompt', () => {
+    const add = wrapper.find(".add-client")
+    add.simulate('click');
+    const prompt = wrapper.find(".prompt")
+    expect(prompt.hasClass("show")).to.equal(true)
+ });
